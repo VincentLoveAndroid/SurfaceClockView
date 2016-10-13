42a1cc44b5109f925de590c19729a13d2c4114bb
VincentLoveAndroid <674928145@qq.com>
2016-10-13 17:04:47 +0800

    first commit

---
+++ app/src/main/java/com/example/mingren/surfaceclockview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mingren.surfaceclockview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by vincent on 2016/10/10.
+ * email-address 674928145@qq.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
